029c3eafd9c380f83b1fba9a4047263229e18459
Alfonso <alfcrisci@gmail.com>
2016-07-06 12:20:20 +0200

    Update experimental.sh

--- experimental.sh
+++ experimental.sh
@@ -1,1 +1,14 @@
 sudo apt-get install libspatialite-dev
+
+# https://github.com/ghaering/pysqlite
+# https://pysqlite.readthedocs.io/en/latest/sqlite3.html
+
+
+sudo [easy_install pyspatialite|pip install pyspatialite]
+
+# http://false.ekta.is/2011/04/pyspatialite-spatial-queries-in-python-built-on-sqlite3/
+
+# https://gist.github.com/happysundar/6181707
+
+sudo apt-get install libgeos-dev libproj-dev python-dev 
+
